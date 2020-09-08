c6a0cdbe6bc64838811641f4ccaf4963f2530d4d
Ronen Tadmor <33766770+amnon18@users.noreply.github.com>
2020-09-07 21:08:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -115,6 +115,14 @@ socket.on('add user', function (username) {
 	});
 });
 
+// Allow access control for web requests
+app.use(function(req, res, next) {
+ res.header("Access-Control-Allow-Origin", "*");
+res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+next();
+});
 
 server.listen(port, function(){
   console.log('listening on *:' + port);
