13413dc91afcb8fe83562d9eb7a4f6a9bd7f63f5
Ronen Tadmor <33766770+amnon18@users.noreply.github.com>
2020-09-07 21:11:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -116,6 +116,7 @@ socket.on('add user', function (username) {
 });
 
 // Allow access control for web requests
+
 app.use(function(req, res, next) {
  res.header("Access-Control-Allow-Origin", "*");
 res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
