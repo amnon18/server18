23b9b82a5763c8c7c51f2b407056b7d7ac0e8418
Ronen Tadmor <33766770+amnon18@users.noreply.github.com>
2023-10-28 21:38:12 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
+var app = require('express');
+var server = require('http');
+var io = require('socket.io')            //var io = require('socket.io')(http);
+
+
 
 const app = express();
 const server = http.createServer(app);
