92cb03641a1b03e256c32c259ba41c768926c9a6
Ronen Tadmor <33766770+amnon18@users.noreply.github.com>
2023-10-31 19:34:22 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -154,3 +154,28 @@ function catchAllEventListener(socket, eventName, ...args) {
   
   // You can add custom logic here to handle any event as needed
 }
+
+io.on("connection", (socket) => {
+  console.log("initial transport", socket.conn.transport.name); // prints "polling"
+
+  socket.conn.once("upgrade", () => {
+    // called when the transport is upgraded (i.e. from HTTP long-polling to WebSocket)
+    console.log("upgraded transport", socket.conn.transport.name); // prints "websocket"
+  });
+
+  socket.conn.on("packet", ({ type, data }) => {
+    console.log("Receieved"); // prints "websocket"
+  });
+
+  socket.conn.on("packetCreate", ({ type, data }) => {
+    // called for each packet sent
+  });
+
+  socket.conn.on("drain", () => {
+    // called when the write buffer is drained
+  });
+
+  socket.conn.on("close", (reason) => {
+    // called when the underlying connection is closed
+  });
+});
