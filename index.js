96650ad129a7306e763794dbe1b192f344cf0623
Ronen Tadmor <33766770+amnon18@users.noreply.github.com>
2023-11-01 00:17:08 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -195,6 +195,7 @@ io.on('connection', (socket) => {
 
   console.log('Connected');
   console.log(socket.id);
+  console.log(socket.data);
   console.log("JWT token test: ",socket.handshake.headers)
 
   socket.on('event_name', (data, res) => {
