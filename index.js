08539d58076e7e89c1b42c0354d47fa57b3e27cd
Ronen Tadmor <33766770+amnon18@users.noreply.github.com>
2023-11-01 21:34:21 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -28,3 +28,26 @@ const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+io.use((socket, next) => {
+  const originalOnevent = socket.onevent;
+  socket.onevent = function (packet) {
+    const [eventName, ...args] = packet.data;
+    console.log(`Event received: ${eventName}`);
+
+    // Catch-all event listener
+    catchAllEventListener(socket, eventName, ...args);
+
+    // Call the original onevent function
+    originalOnevent.call(this, packet);
+  };
+  next();
+});
+
+function catchAllEventListener(socket, eventName, ...args) {
+  console.log('Catch-all event listener triggered');
+  console.log('Event Name:', eventName);
+  console.log('Arguments:', args);
+  
+  // You can add custom logic here to handle any event as needed
+}
