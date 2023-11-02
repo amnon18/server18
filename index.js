b2ccfef6a0d3e21407f955faa027879a85255864
Ronen Tadmor <33766770+amnon18@users.noreply.github.com>
2023-11-01 21:40:59 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -6,17 +6,22 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+//app.get('/', (req, res) => {
+//  res.send('Hello World!');
+//});
 
 io.on('connection', (socket) => {
   console.log('A user connected');
 
-  // Listen for messages from this specific client
-  socket.on('message', (message) => {
-    console.log('Message Received:', message);
+ socket.onAny((eventName, ...args) => {
+    console.log('Event Received:', eventName);
+    console.log('Data:', ...args);
   });
+  
+  // Listen for messages from this specific client
+  //socket.on('message', (data) => {
+  //  console.log('Message Received:', data);
+  //});
 
   // Listen for this specific client to disconnect
   socket.on('disconnect', () => {
@@ -29,25 +34,3 @@ server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-io.use((socket, next) => {
-  const originalOnevent = socket.onevent;
-  socket.onevent = function (packet) {
-    const [eventName, ...args] = packet.data;
-    console.log(`Event received: ${eventName}`);
-
-    // Catch-all event listener
-    catchAllEventListener(socket, eventName, ...args);
-
-    // Call the original onevent function
-    originalOnevent.call(this, packet);
-  };
-  next();
-});
-
-function catchAllEventListener(socket, eventName, ...args) {
-  console.log('Catch-all event listener triggered');
-  console.log('Event Name:', eventName);
-  console.log('Arguments:', args);
-  
-  // You can add custom logic here to handle any event as needed
-}
