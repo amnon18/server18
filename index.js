0859b2f8c6cda0fc565eadf8c715ee4ca6305b6a
Ronen Tadmor <33766770+amnon18@users.noreply.github.com>
2023-11-02 16:29:44 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -4,20 +4,28 @@ const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: '*',
-  },
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+  socket.onAny((eventName, ...args) => {
+    const profile = args.find(arg => arg.profile);
+    if(profile) {
+      console.log(`Received message from ${profile.name}:`);
+    } else {
+      console.log(`Received anonymous message:`);
+    }
+    console.log(`Event: ${eventName}, Message:`, args);
+  });
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
 });
 
-const PORT = process.env.PORT || 4000;
-server.listen(PORT, () => {
-  console.log(`listening on *:${PORT}`);
+server.listen(4000, () => {
+  console.log('listening on *: 4000');
 });
