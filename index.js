97605c4d8bbf4b300fcd6d40c81cab2269b86420
Ronen Tadmor <33766770+amnon18@users.noreply.github.com>
2023-11-02 16:23:00 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
-
-app.get('/', (req, res) => {
-  res.send('WebSocket Server is running');
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
 });
 
 io.on('connection', (socket) => {
-  console.log('A client connected');
-
+  console.log('a user connected');
   socket.on('disconnect', () => {
-    console.log('Client disconnected');
-  });
-
-  socket.on('message', (message) => {
-    console.log('Message from Wemos:', message);
+    console.log('user disconnected');
   });
 });
 
 const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`listening on *:${PORT}`);
 });
