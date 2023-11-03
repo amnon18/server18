feee646465bf5084385652677eed3c9acb6e29aa
Ronen Tadmor <33766770+amnon18@users.noreply.github.com>
2023-11-02 21:43:11 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,162 +1,34 @@
-var app = require('express')();
-var server = require('http').createServer(app);    //var http = require('http').Server(app);
-var io = require('socket.io')(server);             //var io = require('socket.io')(http);
-var port = process.env.PORT || 4000;
+var bodyParser = require("body-parser");
+const express = require('express'); //express framework to have a higher level of methods
+const app = express(); //assign app variable the express class/method
+var http = require('http');
+var path = require("path");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+const server = http.createServer(app);//create a server
+var io = require('socket.io').listen(server);
+port=80
+server.listen(port);
 
-//************************************************
-var numUsers = 0;
-var clients = []
-var clientsession = []
-var clientcount = 0;
-var users = {};
-var userNumber = 1;
-var ausr = ''; //Active user
-//************************************************
+app.use('/', express.static(path.join('public')));
 
-// Allow access control for web requests
-
-app.use(function(req, res, next) {
- res.header("Access-Control-Allow-Origin", "*");
-res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-next();
+app.get('/', function (req, res) {
+  res.sendFile(path.join('public','index.html'));
 });
 
+io.sockets.on('connection', function (socket) {
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+  console.log(`[server][info] Socket ${socket.id} connected.`);
 
-app.get('/base', function(req, res){
-  res.sendFile(__dirname + '/base.html');
-});
+  socket.on('disconnect', () => {
+    console.log(`[server][info] Socket ${socket.id} disconnected.`);
+  });
 
-
-
-io.on('connection', function(socket){
-	socket.on('delayer', function(msg){ 
-		delay(1000);
-		console.log('Sending...');
-		io.emit(ausr, msg);
-	});
-	
-});
-
-function delay(ms) {
-			var cur_d = new Date();
-			var cur_ticks = cur_d.getTime();
-			var ms_passed = 0;
-			while(ms_passed < ms) {
-				var d = new Date();  // Possible memory leak?
-				var ticks = d.getTime();
-				ms_passed = ticks - cur_ticks;
-				// d = null;  // Prevent memory leak?
-    	    }
-	    }
-
-
-io.on('connection', function(socket){
-	socket.on('remote', function(msg){ 
-		if (msg.substr(0,2) == '$S'){
-			ausr = msg.substr(2,4);
-			return;
-		}
-		if (msg.substr(0,2) == '$L'){
-		    io.emit('remote', "Clients online: "+numUsers);
-			for (var x=0;x<=clientsession.length-1;x++) {
-				io.emit('remote',  "Client ID: "+clients[x]+":"+clientsession[x]);
-			}
-		}else{
-			io.emit(ausr, msg);
-		}
-	});
-	// Receieved info from remote and sends to base
-
-	socket.on('message', function(msg){
-		io.emit('1969', msg);
-		io.emit('remote', msg);
-		console.log (msg);
-		return;
-	});
-});
-
-// Receieved info from remote and sends to base
-io.on('connection', function(socket){
-	socket.on('base', function(msg){
-		io.emit('remote', msg);
-		return;
-});
-
-
-socket.on('disconnect', function () {
-	io.emit('remote', 'Customer left remote comm. '+this.id);
-	for (var r=0;r<=clientsession.length-1;r++){
-		if (clientsession[r] == this.id){
-			clientsession.splice(r, 1); 				
-			clients.splice(r, 1); 				
-		}
-	}
-	socket.disconnect(true);
-	numUsers--;
-	clientcount--;
-	if (numUsers<0) numUsers=0;
-	if (clientcount<0) clientcount=0;
-});
-
-socket.on('new', function(username) {
-	
-	addedUser = true;
-	socket.username = username;
-	io.emit('remote', socket.username+' joined remote service.');
-	numUsers++;
-	io.emit('remote', 'Number of clients online: '+numUsers);
-	io.emit('rid', username);
-	console.log(username);
-	clients[clientcount] =  username;
-	clientsession[clientcount] =  socket.id;
-	clientcount++;
-});
-
-// when the client emits 'add user', this listens and executes
-socket.on('add user', function (username) {
-	if (addedUser) return;
-	// we store the username in the socket session for this client
-	socket.username = username;
-	++numUsers;
-	addedUser = true;
-	socket.emit('remote', {
-	  numUsers: numUsers
-	});
-	io.emit('remote', 'Customer joined remote com.');
-	 ++numUsers;
-	});
-});
-
-
-server.listen(port, function(){
-  console.log('listening on *:' + port);
-});
-
-io.use((socket, next) => {
-  const originalOnevent = socket.onevent;
-  socket.onevent = function (packet) {
-    const [eventName, ...args] = packet.data;
-    console.log(`Event received: ${eventName}`);
-
-    // Catch-all event listener
-    catchAllEventListener(socket, eventName, ...args);
-
-    // Call the original onevent function
-    originalOnevent.call(this, packet);
-  };
-  next();
-});
-
-function catchAllEventListener(socket, eventName, ...args) {
-  console.log('Catch-all event listener triggered');
-  console.log('Event Name:', eventName);
-  console.log('Arguments:', args);
-  
-  // You can add custom logic here to handle any event as needed
-}
+  socket.on('arduino_measure', function(data) {
+      message = JSON.stringify(data);
+      var time = Math.round(new Date().getTime() / 1000);
+      var newmessage = message.replace('{','{"date_mesure":'+time+',');
+      console.log(newmessage);
+      io.emit('measure', newmessage);
+  });
+})
